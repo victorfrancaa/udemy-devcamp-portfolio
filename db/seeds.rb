a8a7e039ce3b0bf36a8f161fd44cd24f3e7be4b3
Victor Cesar de Franca Lima <vcfl@BREJOES-011075.local>
2019-05-20 15:03:24 -0300

    Crud Portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ puts "5 skills created"
                 subtitle: "My great service",
                 body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 main_image: "http://placehold.it/600x400",
-                thumb_image: "http://placehold.it/350x200"
+                thumg_image: "http://placehold.it/350x200"
   )
 end
 
