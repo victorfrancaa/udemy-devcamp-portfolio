21e1552cb6a6987a9d58baaa1ebd67b88cb15cdf
Victor Cesar de Franca Lima <vcfl@BREJOES-011075.local>
2019-05-22 16:23:06 -0300

    Add validacoes em Blog e Portfolio

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1}
+
+  validates_presence_of :title, :body
 end
--- app/models/portifolio.rb
+++ app/models/portifolio.rb
@@ -1,2 +1,3 @@
 class Portifolio < ApplicationRecord
+  validates_presence_of :title, :body, :main_image, :thumg_image
 end
