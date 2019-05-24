fd2a200135b808a2cc7f94472ae69342a5223c44
Victor Cesar de Franca Lima <vcfl@BREJOES-011075.local>
2019-05-23 22:46:28 -0300

    log in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login' , sign_out: 'logout', sign_up: 'register' }
   resources :portifolios
   resources :blogs do
     member do
