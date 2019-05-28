353c2c65c3dccd30d53241f38dfb0e73aaf202e9
Victor Lima <victor_lima@apple.com>
2019-05-28 08:13:49 -0300

    Imp Content Datasource

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe + 
+      (link_to "Login", new_user_session_path)
+    end
+  end
+
+  def sample_helper
+    "<p>My helper</p>".html_safe
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      content = "<p>Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout</p>"
+      content_tag(:p, content, class: "source-content")
+    end
+  end
+
 end
